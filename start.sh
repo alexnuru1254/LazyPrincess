66c2d5855aca34dccf06772a7bdafeec3f890042
alexnuru1254 <161445157+alexnuru1254@users.noreply.github.com>
2024-02-29 02:49:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/alexnuru1254/LazyPrincess
+git clone $UPSTREAM_REPO /LazyPrincess
 cd /LazyPrincess
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
